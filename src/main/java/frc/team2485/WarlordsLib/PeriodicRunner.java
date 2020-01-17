fddc285a9d86d083ebcc06f99629c40d93783c5d
Nathan Sariowan <ncsariowan@gmail.com>
2020-01-16 19:35:02 -0800

    Document periodic runner

--- src/main/java/frc/team2485/WarlordsLib/PeriodicRunner.java
+++ src/main/java/frc/team2485/WarlordsLib/PeriodicRunner.java
@@ -2,6 +2,9 @@ package frc.team2485.WarlordsLib;
 
 import edu.wpi.first.wpilibj.Timer;
 
+/**
+ * A simple class for running things periodically for given times greater than 0.02 seconds.
+ */
 public class PeriodicRunner {
 
     private double _seconds;
@@ -10,18 +13,22 @@ public class PeriodicRunner {
 
     private Timer _timer;
 
+    /**
+     * Runs a given method periodically by a given amount of seconds.
+     * @param runnable the method run
+     * @param seconds seconds in between each call.
+     */
     public PeriodicRunner(Runnable runnable, double seconds) {
         this._runnable = runnable;
         this._seconds = seconds;
         this._timer = new Timer();
         _timer.reset();
-    }
-
-    public void init() {
-        _timer.reset();
         _timer.start();
     }
 
+    /**
+     * Run the periodic runner, which will only run the runner every given amount of seconds.
+     */
     public void run() {
         if (_timer.get() > _seconds) {
             _runnable.run();
