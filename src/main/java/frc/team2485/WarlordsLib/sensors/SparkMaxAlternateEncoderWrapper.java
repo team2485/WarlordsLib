428ce6968cf286524844d1a93bacdf62564538fa
Hanna Dorsey <ncsariowan@gmail.com>
2020-01-19 14:53:12 -0800

    Document SparkMaxAlternativeEncoderWrapper

--- src/main/java/frc/team2485/WarlordsLib/sensors/SparkMaxAlternateEncoderWrapper.java
+++ src/main/java/frc/team2485/WarlordsLib/sensors/SparkMaxAlternateEncoderWrapper.java
@@ -21,25 +21,41 @@ public class SparkMaxAlternateEncoderWrapper implements Encoder {
         this(new CANSparkMax(deviceId, CANSparkMaxLowLevel.MotorType.kBrushless), pulsesPerRevolution);
     }
 
+    /**
+     * Get position in rotations * distance per revolution
+     * @return distance
+     */
     @Override
     public double getPosition() {
         return encoder.getPosition();
     }
 
+    /**
+     * Reset position
+     * @param position position to reset to in distance taking into account conversion factor.
+     */
     @Override
     public void setPosition(double position) {
         handleCANError(encoder.setPosition(position));
     }
 
+    /**
+     * Set the distance per revolution of the encoder
+     * @param distance distance per rev
+     */
     @Override
     public void setDistancePerRevolution(double distance) {
         handleCANError(encoder.setPositionConversionFactor(distance));
         handleCANError(encoder.setVelocityConversionFactor(distance));
     }
 
+    /**
+     * Get velocity of the encoder is distance per second
+     * @return distance per second
+     */
     @Override
     public double getVelocity() {
-        return encoder.getVelocity() * 60; // from distance per minute to distance per second
+        return encoder.getVelocity() / 60; // from distance per minute to distance per second
     }
 
     private void handleCANError(CANError error) {
@@ -48,6 +64,18 @@ public class SparkMaxAlternateEncoderWrapper implements Encoder {
         }
     }
 
+    /**
+     * Sets this encoder to be inverted
+     * @param inverted whether encoder is inverted
+     */
+    public void setInverted(boolean inverted) {
+        encoder.setInverted(inverted);
+    }
+
+    /**
+     * Get this encoder
+     * @return the CANEncoder
+     */
     public CANEncoder getEncoder() {
         return encoder;
     }
