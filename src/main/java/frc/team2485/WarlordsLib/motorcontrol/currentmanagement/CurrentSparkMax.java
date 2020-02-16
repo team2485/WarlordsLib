72738f99f819ec4facd6c8e9397acdb81ef4dfee
Nathan Sariowan <ncsariowan@gmail.com>
2020-02-15 18:56:56 -0800

    Current spark max docs

--- src/main/java/frc/team2485/WarlordsLib/motorcontrol/currentmanagement/CurrentSparkMax.java
+++ src/main/java/frc/team2485/WarlordsLib/motorcontrol/currentmanagement/CurrentSparkMax.java
@@ -15,10 +15,18 @@ public class CurrentSparkMax extends PIDSparkMax implements Manageable {
         this.m_adjustedMaxCurrent = m_absoluteMaxCurrent;
     }
 
+    /**
+     * Set a value between -1 and 1 scaled by absolute max current
+     * @param pwm value between -1 and 1
+     */
     public void setPWM(double pwm) {
         this.setCurrent(pwm * this.m_absoluteMaxCurrent);
     }
 
+    /**
+     * Set directly a current target to the current pid
+     * @param current current in Amps
+     */
     public void setCurrent(double current) {
         if(current > m_adjustedMaxCurrent) {
             this.runPID(m_adjustedMaxCurrent);
--- src/main/java/frc/team2485/WarlordsLib/motorcontrol/currentmanagement/CurrentTalonSRX.java
+++ src/main/java/frc/team2485/WarlordsLib/motorcontrol/currentmanagement/CurrentTalonSRX.java
@@ -13,10 +13,18 @@ public class CurrentTalonSRX extends PIDTalonSRX implements Manageable {
         this.adjustedMaxCurrent = m_absoluteMaxCurrent;
     }
 
+    /**
+     * Set a value between -1 and 1 scaled by absolute max current
+     * @param pwm value between -1 and 1
+     */
     public void setPWM(double pwm) {
         this.setCurrent(pwm * this.m_absoluteMaxCurrent);
     }
 
+    /**
+     * Set directly a current target to the current pid
+     * @param current current in Amps
+     */
     public void setCurrent(double current) {
         if(current > adjustedMaxCurrent) {
             this.runPID(adjustedMaxCurrent);
