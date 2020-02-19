d11cbd2fc6e38932ffb9c05e39aefcc4302d249e
Nathan Sariowan <ncsariowan@gmail.com>
2020-02-18 19:15:29 -0800

    Remove setRawPWM from Tunable

--- src/main/java/frc/team2485/WarlordsLib/Tunable.java
+++ src/main/java/frc/team2485/WarlordsLib/Tunable.java
@@ -7,10 +7,4 @@ public interface Tunable {
      * @param enable use this parameter to only enable the motor when this is true.
      */
     void tunePeriodic(boolean enable);
-
-    /**
-     * Set the raw PWM of the subsystem motor. This is for setting the PWM WITHOUT using any current-based pwm.
-     * @param pwm value between -1 and 1
-     */
-    void setRawPWM(double pwm);
 }
