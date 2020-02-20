4332094422a58cfbbf7e51a7692ced95c6834225
Hanna Dorsey <ncsariowan@gmail.com>
2020-02-19 17:32:10 -0800

    Tunable remove boolean

--- src/main/java/frc/team2485/WarlordsLib/Tunable.java
+++ src/main/java/frc/team2485/WarlordsLib/Tunable.java
@@ -3,8 +3,7 @@ package frc.team2485.WarlordsLib;
 public interface Tunable {
 
     /**
-     * Should run periodically and run the motor to tune when enabled
-     * @param enable use this parameter to only enable the motor when this is true.
+     * Should run periodically and run the motor to tune
      */
-    void tunePeriodic(boolean enable);
+    void tunePeriodic();
 }
