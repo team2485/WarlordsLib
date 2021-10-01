1fa4161c502d3cb49c1fa3e47d1b4ba9ff38f6fa
Mark <mrifkin2022@francisparker.org>
2021-10-01 11:00:21 -0700

    Added and tested linear interpolator

---
+++ src/main/java/frc/team2485/WarlordsLib/Point.java
@@ -0,0 +1,15 @@
+package frc.team2485.WarlordsLib;
+
+public class Point implements Comparable<Point> {
+    public final double X;
+    public final double Y;
+
+    public Point(double x, double y) {
+        X = x;
+        Y = y;
+    }
+
+    public int compareTo(Point p) {
+        return Double.compare(this.X, p.X); 
+    }
+}
